580486e7827c727c4d4cf521edf3fb7eb5194d13
Mazen Al Swar <43959520+mazenswar@users.noreply.github.com>
2024-07-12 08:53:42 -0400

    v3.8

--- app/layout.js
+++ app/layout.js
@@ -2,23 +2,19 @@ import "./scss/style.scss";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import Head from "next/head";
 
 export const metadata = {
 	title: "Center for Anxiety and Life Management (Calm Therapy)",
 	description:
-		"Center for Anxiety and Life Management (CALM Therapy) is a professional PLLC specializing in the treatment of anxiety-related disorders, including generalized anxiety disorder, OCD, body dysmorphia, and panic disorder. We use a variety of evidence-based approaches, such as CBT, ACT, and meta-cognitive approaches, to help manage these issues. Currently, we offer virtual therapy sessions for residents of Pennsylvania and New York.",
+		"Center for Anxiety and Life Management (CALM Therapy) specializes in treating anxiety and obsessive compulsive related disorders like GAD, OCD, body dysmorphia, and panic disorder. We use evidence-based approaches, including CBT, ACT, and meta-cognitive techniques. Offering virtual therapy sessions for residents of Pennsylvania and New York.",
+	verification: {
+		google: "HLY4tQ5zcfv4L7ffmk3EhUPPJfnFOITw6Hd8-YAP20Y",
+	},
 };
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<Head>
-				<meta
-					name="google-site-verification"
-					content="HLY4tQ5zcfv4L7ffmk3EhUPPJfnFOITw6Hd8-YAP20Y"
-				/>
-			</Head>
 			<body>
 				<link rel="icon" href="/CALM_E2.png" sizes="any" />
 				<Nav />
