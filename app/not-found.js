9484a42f43a6b30cf7cb63d9ef0709059007e61e
Mazen Al Swar <43959520+mazenswar@users.noreply.github.com>
2024-11-21 16:11:40 -0500

    v5.6

--- app/not-found.js
+++ app/not-found.js
@@ -3,11 +3,13 @@ import Link from "next/link";
 export default function NotFound() {
 	return (
 		<main id="not-found">
-			<h1>404</h1>
-			<h2 className="center-title">Oops, we could not find this page</h2>
-			<div>
-				<Link href="/">Go back to Home</Link>
-			</div>
+			<section className="error-section">
+				<h1>404</h1>
+				<h2 className="center-title">Oops, we could not find this page</h2>
+				<div>
+					<Link href="/">Go back to Home</Link>
+				</div>
+			</section>
 		</main>
 	);
 }
