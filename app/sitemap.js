543e041b6b36c95a5d1684fb250d861a8d443e53
Mazen Al Swar <43959520+mazenswar@users.noreply.github.com>
2025-03-27 15:33:59 -0400

    v7.0

---
+++ app/sitemap.js
@@ -0,0 +1,58 @@
+export default function sitemap() {
+	return [
+		{
+			url: "https://calmtherapy.center",
+			lastModified: new Date(),
+			changeFrequency: "yearly",
+			priority: 1,
+		},
+		{
+			url: "https://calmtherapy.center/blog",
+			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 0.5,
+		},
+		{
+			url: "https://calmtherapy.center/insurance",
+			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 0.5,
+		},
+		{
+			url: "https://calmtherapy.center/specialities",
+			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 0.5,
+		},
+		{
+			url: "https://calmtherapy.center/kap",
+			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 0.5,
+		},
+		{
+			url: "https://calmtherapy.center/faq",
+			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 0.5,
+		},
+		{
+			url: "https://calmtherapy.center/assessments",
+			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 0.5,
+		},
+		{
+			url: "https://calmtherapy.center/resources",
+			lastModified: new Date(),
+			changeFrequency: "monthly",
+			priority: 0.5,
+		},
+		{
+			url: "https://calmtherapy.center/contact",
+			lastModified: new Date(),
+			changeFrequency: "weekly",
+			priority: 0.8,
+		},
+	];
+}
